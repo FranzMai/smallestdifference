6a03b358d8894c732c6f41028960ef1e84c9a61c
FranzMai <f.mailahn@mailbox.org>
2020-04-22 13:31:13 +0200

    Test 3 erstellt

--- src/test/java/de/kohnlehome/SmallestdifferenceTest.java
+++ src/test/java/de/kohnlehome/SmallestdifferenceTest.java
@@ -8,7 +8,7 @@ import static org.mockito.Mockito.*;
 
 
 public class SmallestdifferenceTest {
-    ISmallestdifference smallestdifference;
+    private ISmallestdifference smallestdifference;
     @BeforeEach
     public void init(){
         //Arrange
@@ -17,34 +17,34 @@ public class SmallestdifferenceTest {
 
     @Test
     public void setSmallestdifferenceTest() {
-
+        //Arrange
         int[] array1 = {1,3,15,11,2};
         int[] array2 = {23,127,235,19,8};
         //Act
-        int result =  smallestdifference.smallestdifference(array1,array2);
+        int result = this.smallestdifference.smallestdifference(array1,array2);
         //Assert
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void setSmallestdifferenceTest2() {
-
+        //Arrange
         int[] array1 = {5,6,7};
         int[] array2 = {9,10,11};
         //Act
-        int result =  smallestdifference.smallestdifference(array1,array2);
+        int result =  this.smallestdifference.smallestdifference(array1,array2);
         //Assert
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     public void setSmallestdifferenceTest3() {
-
-        int[] array1 = {1,3,15,11,2};
-        int[] array2 = {23,127,235,19,8};
+        //Arrange
+        int[] array1 = {1};
+        int[] array2 = {1};
         //Act
-        int result =  smallestdifference.smallestdifference(array1,array2);
+        int result =  this.smallestdifference.smallestdifference(array1,array2);
         //Assert
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(0);
     }
 }
